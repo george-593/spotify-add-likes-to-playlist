2381e6cd18a60cc2d3ddcdbb457ffa6b699bc8c8
george-593 <63100333+george-593@users.noreply.github.com>
2023-10-13 21:36:59 +0100

    Catch TimeoutError

--- app.py
+++ app.py
@@ -18,22 +18,30 @@ scope = "user-library-read, playlist-modify-public, playlist-modify-private, pla
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 while True:
-    # Get the user's saved tracks
+    try:
+        # Get the user's saved tracks
+        saved = sp.current_user_saved_tracks()
+        playlist = sp.user_playlist("spotify", PLAYLIST_ID)
 
-    saved = sp.current_user_saved_tracks()
-    playlist = sp.user_playlist("spotify", PLAYLIST_ID)
-
-    # Add any new tracks to the playlists
-    for item in saved["items"]:
-        track = item["track"]
-        if track["id"] not in [t["track"]["id"] for t in playlist["tracks"]["items"]]:
-            log(f"Adding {track['name']} by {track['artists'][0]['name']} to playlist")
-            sp.playlist_add_items(PLAYLIST_ID, [track["id"]])
-            if DELETE_AFTER_ADD:
-                log(f"Deleting {track['name']} from saved tracks")
-                sp.current_user_saved_tracks_delete([track["id"]])
-        else:
-            log(f"{track['name']} by {track['artists'][0]['name']} already in playlist")
+        # Add any new tracks to the playlists
+        for item in saved["items"]:
+            track = item["track"]
+            if track["id"] not in [
+                t["track"]["id"] for t in playlist["tracks"]["items"]
+            ]:
+                log(
+                    f"Adding {track['name']} by {track['artists'][0]['name']} to playlist"
+                )
+                sp.playlist_add_items(PLAYLIST_ID, [track["id"]])
+                if DELETE_AFTER_ADD:
+                    log(f"Deleting {track['name']} from saved tracks")
+                    sp.current_user_saved_tracks_delete([track["id"]])
+            else:
+                log(
+                    f"{track['name']} by {track['artists'][0]['name']} already in playlist"
+                )
+    except TimeoutError:
+        log("Timeout error, retrying on next loop")
 
     # Sleep for 1 min
     log("Loop finished, sleeping for 30 seconds")
